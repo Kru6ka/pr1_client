caa10125ac478f128d51dc4e780bd172f511f9c7
Kru6ka <rty6ueh3eva@gmail.com>
2019-07-02 16:59:46 +0300

    1

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double chislo_na_server;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9./-]");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            vivod.Text = Convert.ToString(chislo_na_server);
+        }
+
+        public void Chislo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double message;
+            message = Convert.ToDouble(chislo.Text);
+            chislo_na_server = message;
+        }
+        static int port = 5000;
+        static string address = "192.168.12.142";
+        static void MainTask(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+                byte[] data = Encoding.Unicode.GetBytes(Convert.ToString(chislo_na_server));
+                socket.Send(data);
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
